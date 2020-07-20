bbf5089cf56f5e0550bd3c84e04d7dba208827ef
Daniel Gleason <danielg@surveymonkey.com>
2020-07-19 23:01:31 -0700

    Changed fonts and added an echo to the start local script

--- start_local.sh
+++ start_local.sh
@@ -1,1 +1,2 @@
+echo "Once the server is up, click this link: http://localhost:8080/"
 docker run --volume="$PWD:/srv/jekyll" -it -p 8080:4000 jekyll/jekyll jekyll serve --watch --force_polling --incremental
