3082ef2f0760ef94166722b7c68e4f2a80581b19
Daniel Gleason <danielg@surveymonkey.com>
2020-07-20 17:32:03 -0700

    Added Atom feed and sitemap plugins

--- start_local.sh
+++ start_local.sh
@@ -1,2 +1,3 @@
 echo "Once the server is up, click this link: http://localhost:8080/"
+rm -rf .jekyll-cache .jekyll-metadata Gemfile.lock
 docker run --volume="$PWD:/srv/jekyll" -it -p 8080:4000 jekyll/jekyll jekyll serve --watch --force_polling --incremental
